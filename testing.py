1b76388a4f1a3bc8965cced8bc372552d47585eb
oreoqupang <wonseok9911@gmail.com>
2020-11-29 07:02:51 +0900

    [Fix] testing.py do_attack

--- testing.py
+++ testing.py
@@ -16,53 +16,44 @@ def do_attack(bytez, bytez_len, result, gradient):
   for idx in range(len(bytez)):
     if result[idx] == 0 or bytez_len[idx] >= first_n_byte: #classfied benign or no padding
       continue
-    
-    byte_len = int(bytez_len[idx])
-    padding_len = min(byte_len + padding_limit, first_n_byte) - byte_len
-    padding = bytez[idx, byte_len:byte_len+padding_len]
+    with torch.no_grad():
+      byte_len = int(bytez_len[idx])
+      padding_len = min(byte_len + padding_limit, first_n_byte) - byte_len
+      padding = bytez[idx, byte_len:byte_len+padding_len]
 
-    for j in range(padding_len):
-      x_j = padding[j]
-      z_j = M[x_j]
+      z = embedding(padding) # padding_len * 8
+      w = -gradient[idx, byte_len:byte_len+padding_len] # padding_len * 8
+      n = w / torch.norm(w, dim=1, p=2)[:, None] # padding_len * 8
 
-      w_j = -gradient[idx, j+byte_len]
-      if torch.norm(w_j, p=2) != 0:
-        n_j =  w_j / torch.norm(w_j, p=2)
-      else:
-        continue
+      ss=torch.bmm(torch.stack([M]*padding_len, dim = 0)-z.unsqueeze(dim=1), n.unsqueeze(dim=2)).squeeze() # ss size = padding_len * 256
+      
+      ds = torch.stack([M]*padding_len, dim = 0)-(torch.bmm(ss.view(padding_len, -1, 1), n.view(padding_len, 1, -1))+z.unsqueeze(dim=1))
+      ds = torch.norm(ds, dim = 2, p = 2)
+      
+      filtered_ds = ((ss>0) * ds)
+      bytez[idx, byte_len:byte_len+padding_len] = torch.where(torch.isnan(filtered_ds[:, 0]), padding, filtered_ds.argmax(dim=1)+1)
 
-      max_distance = -1
-      max_value = None
-      for i in range(256):
-        s_i = n_j.dot(M[i]-z_j)
-        if s_i > 0:
-          d_i = torch.norm(M[i]-(z_j + s_i*n_j), p=2)
-          if max_distance < d_i:
-            max_distance = d_i
-            max_value = i
-
-      if max_value != None:
-        padding[j] = max_value
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+print(device)
 malconv = MalConv(channels=256, window_size=512, embd_size=8).to(device)
 weights = torch.load('./malconv.checkpoint', map_location='cpu')
 malconv.load_state_dict(weights['model_state_dict'])
 malconv.eval()
 
-test_num = 2
+test_num = 4
+batch_size = 64
 loop_num = 20
 first_n_byte = 1000000
 padding_limit = 1000
-file_names = os.listdir('/Users/choiwonseok/samples/malwares')
+file_names = os.listdir('/home/choiwonseok/sample/malwares_2017')
 
-test_loader = DataLoader(ExeDataset(file_names, "/Users/choiwonseok/samples/malwares/", 
+test_loader = DataLoader(ExeDataset(file_names, "/home/choiwonseok/sample/malwares_2017/", 
   [1]*11000, padding_limit, first_n_byte),
-  batch_size=4, shuffle=True)
+  batch_size=batch_size, shuffle=True)
 
 embedding = malconv.embd
-M = embedding(torch.arange(0, 257))
+M = embedding(torch.arange(1, 257))
 
 test_cnt = 0
 attack_cnt = 0
@@ -89,7 +80,7 @@ for bytez, label, bytez_len in test_loader:
     if i == 0:
       attack_cnt += torch.count_nonzero(torch.logical_and(init_result, bytez_len.squeeze() < first_n_byte))
       result = init_result
-      init_malness.backward(torch.Tensor([1]*4))
+      init_malness.backward(torch.Tensor([1]*batch_size))
 
     do_attack(bytez, bytez_len, result, embed.grad)
 
@@ -100,7 +91,7 @@ for bytez, label, bytez_len in test_loader:
     result = malness > 0.5
     print(malness)
 
-    malness.backward(torch.Tensor([1]*4))
+    malness.backward(torch.Tensor([1]*batch_size))
 
   success_cnt += torch.count_nonzero(init_result) - torch.count_nonzero(result)
 
@@ -110,7 +101,7 @@ print(f"Success count : {success_cnt}")
 print(f"SR : {success_cnt/attack_cnt*100}%")
 
 with open(f"test_result({padding_limit})", "wt") as output:
-  output.write("--------Test Reslut----------")
-  output.write(f"Total Attack : {attack_cnt}")
-  output.write(f"Success count : {success_cnt}")
-  output.write(f"SR : {success_cnt/attack_cnt*100}%")
+  output.write("--------Test Reslut----------\n")
+  output.write(f"Total Attack : {attack_cnt}\n")
+  output.write(f"Success count : {success_cnt}\n")
+  output.write(f"SR : {success_cnt/attack_cnt*100}%\n")
